d74079c95fc178f21c9c548ccb5900fe7767af2a
Pedro Werkhaizer <pedro.werkhaizer@gmail.com>
2024-10-04 23:57:05 -0300

    Middleware and login (server actions)

---
+++ routes.ts
@@ -0,0 +1,23 @@
+/** An array of routes that are accessible to the public
+ * There routes do not require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes: string[] = ["/"];
+
+/** An array of routes that are used for authentication
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes: string[] = ["/auth/login", "/auth/register"];
+
+/** The prefix for API authentication routes
+ * Routes that starts with this prefix are used for API autenthicaition purposes
+ * @type {string}
+ */
+export const apiAuthPrefix: string = "/api/auth";
+
+/** The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT: string = "/settings";
