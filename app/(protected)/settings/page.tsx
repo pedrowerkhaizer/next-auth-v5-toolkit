99d9be295c1c461a4f5c4907430cdc1681977a08
Pedro Werkhaizer <pedro.werkhaizer@gmail.com>
2024-10-20 18:07:00 -0300

    Signing out client side

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,19 +1,20 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-const SettingsPage = async () => {
-  const session = await auth();
+import { useSession, signOut } from "next-auth/react";
+
+const SettingsPage = () => {
+  const session = useSession();
+
+  const onClick = () => {
+    signOut();
+  };
 
   return (
     <div>
       {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
+      <button type="submit" onClick={onClick}>
+        Sign out
+      </button>
     </div>
   );
 };
